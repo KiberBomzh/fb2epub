3b0136e6c6635b684103d788055448e640eec077
KiberBomzh <klimenokandrej193@gmail.com>
2025-12-30 18:51:05 +0200

    added fb2_parser.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,18 @@
-// Новые модули и всю структуру писать и объявлять здесь
+mod fb2_parser;
+
+use std::path::PathBuf;
+
+
+pub fn start(book: &str) {
+    let path = PathBuf::from(book);
+    
+    // Проверки
+    if !path.exists() {
+        panic!("There's no such path: {:?}!", path)
+    } else if path.extension().unwrap() != "fb2" {
+        panic!("The file {:?} isn't fb2!", path)
+    };
+    
+    
+    fb2_parser::get_data(&path);
+}
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,24 @@
 // Вызовы отсюда нужно не через crate:: делать
 // а через fb2epub:: потому что это библиотека
 
+use clap::{Command, Arg};
+
+
 fn main() {
-	
+	let matches = Command::new("fb2epub")
+	    .version(env!("CARGO_PKG_VERSION"))
+        .author("KiberBomzh")
+        .about("Converter fb2 to epub")
+        .arg(
+            Arg::new("input")
+                .help("A book in fb2")
+                .required(true)
+                .index(1)
+        )
+        .get_matches();
+        
+    match matches.get_one::<String>("input") {
+        Some(input) => fb2epub::start(&input),
+        None => println!("{}", "Must be at least one input!")
+    }
 }
