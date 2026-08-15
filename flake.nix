9f1e23ee375a76d10083aef5ced16b43f96aeae0
KiberBomzh <klimenokandrej193@gmail.com>
2026-08-16 02:09:06 +0300

    added flake.nix

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+	inputs = {
+		nixpkgs.url = "nixpkgs/nixos-unstable";
+		flake-utils.url = "github:numtide/flake-utils";
+	};
+
+	outputs = { self, nixpkgs, flake-utils, ... }:
+	flake-utils.lib.eachDefaultSystem (system:
+		let 
+			pkgs = import nixpkgs { inherit system; };
+		in {
+			packages.default = pkgs.rustPlatform.buildRustPackage {
+				pname = "fb2epub";
+				version = "0.2.0";
+
+				src = ./.;
+
+				cargoLock.lockFile = ./Cargo.lock;
+			};
+
+			devShells.default = pkgs.mkShell {
+				buildInputs = with pkgs; [
+					rustc
+					cargo
+				];
+
+				shellHook = ''
+					export HOME="$PWD/.nix-cache"
+					export CARGO_TERM_COLOR=always
+				'';
+			};
+		}
+	);
+}
